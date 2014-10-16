885c21c009d35edff946b75fe1ade3ecfc71b052
YenChenLin <yenchenlin1994@gmail.com>
2014-10-16 22:03:58 +0800

    added side-bar line

--- HorizontalParallaxScrollView/ViewController.swift
+++ HorizontalParallaxScrollView/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController, UIScrollViewDelegate {
     let MAX_BACKGROUND_SCROLL_DISTANCE: CGFloat = 150
-    //let SIDE_BAR_WIDTH: CGFloat = 20
-    @IBOutlet var scrollView: UIScrollView!
+    let SIDE_BAR_WIDTH: CGFloat = 2
+    
+    var scrollView = UIScrollView()
     
     var pageView1 = UIScrollView()
     var pageView2 = UIScrollView()
@@ -26,33 +27,35 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         var numberOfPages: CGFloat = 3
         var i: CGFloat = 0
         
-        
-        self.scrollView.pagingEnabled = true
-        self.scrollView.frame = CGRectMake(0, 0, self.scrollView.frame.width, self.scrollView.frame.height)
+        self.scrollView.frame = CGRectMake(0, 0, self.view.frame.width + 2*SIDE_BAR_WIDTH, self.view.frame.height)
         self.scrollView.contentSize = CGSize(width: scrollView.frame.width * numberOfPages, height: scrollView.frame.height)
-        
+        self.scrollView.pagingEnabled = true
+        self.scrollView.delegate = self
+        self.view.addSubview(scrollView)
         self.setPageView()
-        
-        
+        //print("contentSize: \(self.scrollView.contentSize.width)\n")
+        print("scrollView: \(self.scrollView.frame.width)\n")
+        //print("pageView: \(self.pageView2.frame.width)\n")
+        //print("pageViewContent: \(self.pageView3.frame.origin.x)\n")
     }
     
     func setPageView() {
         
-        pageView1.contentSize = CGSize(width: scrollView.frame.width + 2*MAX_BACKGROUND_SCROLL_DISTANCE, height: scrollView.frame.height)
-        pageView1.frame = CGRectMake(0, 0, self.scrollView.frame.width, self.scrollView.frame.height)
+        pageView1.contentSize = CGSize(width: self.view.frame.width + 2*MAX_BACKGROUND_SCROLL_DISTANCE, height: scrollView.frame.height)
+        pageView1.frame = CGRectMake(0, 0, self.view.frame.width, self.scrollView.frame.height)
         pageView1.setContentOffset(CGPointMake(MAX_BACKGROUND_SCROLL_DISTANCE, 0), animated: false)
         self.pageView1.userInteractionEnabled = false
         self.scrollView.addSubview(pageView1)
         
         
-        pageView2.contentSize = CGSize(width: scrollView.frame.width + 2*MAX_BACKGROUND_SCROLL_DISTANCE, height: scrollView.frame.height)
-        pageView2.frame = CGRectMake(self.scrollView.frame.width, 0, self.scrollView.frame.width, self.scrollView.frame.height)
+        pageView2.contentSize = CGSize(width: self.view.frame.width + 2*MAX_BACKGROUND_SCROLL_DISTANCE, height: scrollView.frame.height)
+        pageView2.frame = CGRectMake(self.scrollView.frame.width, 0, self.view.frame.width, self.scrollView.frame.height)
         pageView2.setContentOffset(CGPointMake(MAX_BACKGROUND_SCROLL_DISTANCE, 0), animated: false)
         self.pageView2.userInteractionEnabled = false
         self.scrollView.addSubview(pageView2)
         
-        pageView3.contentSize = CGSize(width: scrollView.frame.width + 2*MAX_BACKGROUND_SCROLL_DISTANCE, height: scrollView.frame.height)
-        pageView3.frame = CGRectMake(self.scrollView.frame.width*2, 0, self.scrollView.frame.width, self.scrollView.frame.height)
+        pageView3.contentSize = CGSize(width: self.view.frame.width + 2*MAX_BACKGROUND_SCROLL_DISTANCE, height: scrollView.frame.height)
+        pageView3.frame = CGRectMake(self.scrollView.frame.width*2, 0, self.view.frame.width, self.scrollView.frame.height)
         pageView3.setContentOffset(CGPointMake(MAX_BACKGROUND_SCROLL_DISTANCE, 0), animated: false)
         self.pageView3.userInteractionEnabled = false
         self.scrollView.addSubview(pageView3)
@@ -79,6 +82,7 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     
     func scrollViewDidScroll(scrollView: UIScrollView) {
         var ratio = self.scrollView.contentOffset.x / self.scrollView.frame.width
+        
         if ratio > -1 && ratio < 1 {
             self.pageView1.setContentOffset(CGPointMake(MAX_BACKGROUND_SCROLL_DISTANCE - ratio*MAX_BACKGROUND_SCROLL_DISTANCE, 0), animated: false)
         }
@@ -88,7 +92,8 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         if ratio > 1 && ratio < 3 {
             self.pageView3.setContentOffset(CGPointMake(MAX_BACKGROUND_SCROLL_DISTANCE - (ratio-2)*MAX_BACKGROUND_SCROLL_DISTANCE, 0), animated: false)
         }
-        print("\(ratio)\n")
+        print("\(self.scrollView.contentOffset.x)\n")
+        //print("\(ratio)\n")
     }
 
     override func didReceiveMemoryWarning() {
