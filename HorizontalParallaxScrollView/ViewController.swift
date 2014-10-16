132325cb6d12afdd10d4854f6d14eafd5512ab3d
YenChenLin <yenchenlin1994@gmail.com>
2014-10-16 11:12:22 +0800

    Initial Commit

---
+++ HorizontalParallaxScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HorizontalParallaxScrollView
+//
+//  Created by 林彥辰 on 2014/10/16.
+//  Copyright (c) 2014年 iTaoyuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
